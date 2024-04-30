1c8ad7c0aa8744c4c34fffa955d44309fa1ca5b0
Md Rabby <ablazerabbi159@gmail.com>
2024-05-01 00:33:30 +0600

    premitive data type

---
+++ typescript/src/basic-datatype.ts
@@ -0,0 +1,23 @@
+//basic datatypes 
+
+//number
+let phoneNumber : number = 1254786;
+
+//string
+let firstName : string = 'rabby';
+
+//boolean
+let isHere : boolean = true;
+
+//null 
+let r : null = null;
+
+//undefine 
+let value : undefined = undefined;
+
+//typescript power
+let a : number;
+
+// a = 'aaaaaah!', [showing error because we define 'a' is a number type data.]
+// a = false, [showing error because we define 'a' is a number type data.]
+a = 214587
---
+++ typescript/src/test.ts
@@ -0,0 +1,1 @@
+let test = 'testing conversion ts to js';
