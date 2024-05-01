b6e4e0d0aa236e7d9fc812874d571771463c7ede
Md Rabby <ablazerabbi159@gmail.com>
2024-05-01 14:48:56 +0600

    object destructing

---
+++ typescript/src/destructuring.ts
@@ -0,0 +1,34 @@
+{
+
+
+    //Object Destructing 
+
+    const myInfo = {
+        age : 24,
+        name : {
+            firstName : 'Md',
+            lastName : 'Rabby'
+        },
+        phoneNumber : 245785545
+    };
+
+    const {age, name : {firstName}} = myInfo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
