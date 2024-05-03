3703a9d5c65786ab8fd95b5b42e424e424b94c41
Md Rabby <ablazerabbi159@gmail.com>
2024-05-03 23:06:07 +0600

    type assertion

--- typescriptAdvance/src/type-assertion.ts
+++ typescriptAdvance/src/type-assertion.ts
@@ -2,3 +2,18 @@
  * Types assertion/ Types Narrowing
  */
 
+// let anything : any;
+
+// anything = 'Types assertion';
+
+// anything = 21547;
+
+// (anything as number).toFixed();
+
+let anything : any;
+
+anything = 'Types assertion';
+
+anything = 21547;
+
+(anything as string).
