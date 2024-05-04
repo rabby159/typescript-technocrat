e7cf9e2ffa867eb104fc00d8096bcf2138a1db59
Md Rabby <ablazerabbi159@gmail.com>
2024-05-04 15:50:10 +0600

    omit types

--- typescriptAdvance/src/utilityTypes.ts
+++ typescriptAdvance/src/utilityTypes.ts
@@ -10,5 +10,15 @@
     
     type TodoPreview = Pick<Todo, "title" | "completed">;
 
+    //Omit Types
+    interface Todo {
+        title: string;
+        description: string;
+        completed: boolean;
+    }
+    
+    type TodoWithoutDescription = Omit<Todo, "description">;
+
+
     
 }
