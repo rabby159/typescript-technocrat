a48d22d5742653c619576eb2b41238c7f4d295c0
Md Rabby <ablazerabbi159@gmail.com>
2024-05-06 03:19:06 +0600

    add comment every line

--- oop/accessModifire.ts
+++ oop/accessModifire.ts
@@ -1,40 +1,54 @@
 {
-    //Access Modifiers 
+// Access Modifiers 
 
-    class BankAccount {
-       public readonly no : number;
-       public name : string;
-       private _balance : number;
-       protected details : string;
+// Define a class BankAccount
+class BankAccount {
+    // Public readonly property
+    public readonly no: number;
+    // Public property
+    public name: string;
+    // Private property
+    private _balance: number;
+    // Protected property
+    protected details: string;
 
+    // Constructor
+    constructor(no: number, name: string, _balance: number, details: string) {
+        this.no = no;
+        this.name = name;
+        this._balance = _balance;
+        this.details = details;
+    };
 
-        constructor(no : number, name : string, _balance : number, details : string){
-            this.no = no;
-            this.name = name;
-            this._balance = _balance;
-            this.details = details;
-        };
-
-        addAmount = (amount : number) => {
-            this._balance = this._balance + amount;
-        }
-
-        getBalance(){
-            return this._balance;
-        }
+    // Method to add amount to the balance
+    addAmount = (amount: number) => {
+        this._balance = this._balance + amount;
     }
 
-    class SubBank extends BankAccount {
-        check(){
-            this.details; 
-        }
+    // Method to get the balance
+    getBalance() {
+        return this._balance;
     }
+}
 
-    const poorAccount = new BankAccount(214, 'rabby', 20, 'This is personal only my child can access')
-    poorAccount.addAmount(55);
+// Define a subclass SubBank inheriting from BankAccount
+class SubBank extends BankAccount {
+    // Method to check details (protected property accessible)
+    check() {
+        this.details;
+    }
+}
 
-    const currentBalance = poorAccount.getBalance();
-    console.log(currentBalance);
+// Create an instance of BankAccount
+const poorAccount = new BankAccount(214, 'rabby', 20, 'This is personal only my child can access')
+// Add amount to the account
+poorAccount.addAmount(55);
+
+// Get the current balance
+const currentBalance = poorAccount.getBalance();
+// Print the current balance
+console.log(currentBalance);
+
 
 
 
