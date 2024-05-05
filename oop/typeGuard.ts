21963587d399aa46ea09c99f2e07067118956693
Md Rabby <ablazerabbi159@gmail.com>
2024-05-05 16:04:45 +0600

    type guard using in

--- oop/typeGuard.ts
+++ oop/typeGuard.ts
@@ -7,7 +7,7 @@
 
 type Authentication = string|number;
 
-// Function to add two parameters of type string or number
+// // Function to add two parameters of type string or number
 const add = (param1: Authentication, param2: Authentication): Authentication => {
     // Check if both parameters are numbers using typeof
     if (typeof param1 === 'number' && typeof param2 === 'number') {
@@ -23,6 +23,46 @@ const add = (param1: Authentication, param2: Authentication): Authentication => 
 const result = add("5", "6");
 // Print the result
 console.log(result);
+
+//in guard
+
+// Define type NormalUser
+type NormalUser = {
+    name: string;
+}
+
+// Define type AdminUser
+type AdminUser = {
+    name: string;
+    role: 'Admin';
+}
+
+// Function to get user information
+const getUser = (user: NormalUser | AdminUser) => {
+    // Check if 'role' property exists in user object using 'in' operator
+    if ('role' in user) {
+        // If 'role' property exists, user is an AdminUser
+        console.log(`My name is ${user.name} and my role is ${user.role}`);
+    } else {
+        // If 'role' property doesn't exist, user is a NormalUser
+        console.log(`My name is ${user.name}`);
+    }
+}
+
+// Define a NormalUser object
+const normalUser: NormalUser = {
+    name: 'Mezba bhai',
+};
+
+// Define an AdminUser object
+const adminUser: AdminUser = {
+    name: 'Rabby',
+    role: 'Admin'
+};
+
+// Call the getUser function with an AdminUser object
+getUser(adminUser);
+
 
 
 
