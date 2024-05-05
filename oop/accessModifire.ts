f19b6cd73a1dcf1a3dcaeaf66ac7fbbf7cfa24e4
Md Rabby <ablazerabbi159@gmail.com>
2024-05-06 03:17:24 +0600

    public & private & protected

--- oop/accessModifire.ts
+++ oop/accessModifire.ts
@@ -4,26 +4,33 @@
     class BankAccount {
        public readonly no : number;
        public name : string;
-       private balance : number;
+       private _balance : number;
+       protected details : string;
 
 
-        constructor(no : number, name : string, balance : number){
+        constructor(no : number, name : string, _balance : number, details : string){
             this.no = no;
             this.name = name;
-            this.balance = balance;
+            this._balance = _balance;
+            this.details = details;
         };
 
         addAmount = (amount : number) => {
-            this.balance = this.balance + amount;
+            this._balance = this._balance + amount;
         }
 
         getBalance(){
-            return this.balance;
+            return this._balance;
         }
     }
 
+    class SubBank extends BankAccount {
+        check(){
+            this.details; 
+        }
+    }
 
-    const poorAccount = new BankAccount(214, 'rabby', 20)
+    const poorAccount = new BankAccount(214, 'rabby', 20, 'This is personal only my child can access')
     poorAccount.addAmount(55);
 
     const currentBalance = poorAccount.getBalance();
