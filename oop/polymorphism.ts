5d39714f2dbe31c15a87963bf6c465e84290cf09
Md Rabby <ablazerabbi159@gmail.com>
2024-05-07 01:43:46 +0600

    added comment in every line

--- oop/polymorphism.ts
+++ oop/polymorphism.ts
@@ -1,36 +1,46 @@
 {
     
-    //Polymorphism in OOP
+    // Polymorphism in OOP
 
-    class Person{
-        getSleep(){
-            console.log('I am normal human, So, I am regularly sleep 8 hours!');
-        }
-    };
-
-    class Student extends Person{
-        getSleep(){
-            console.log('I am student, So, I am regularly sleep 6 hours!');
-        }
-    };
-
-    class Developer extends Person{
-        getSleep(){
-            console.log('I am developer, So, I am regularly sleep 4 hours!');
-        }
-    };
-
-    const getSleepHours = (param : Person) =>{
-        param.getSleep();
+// Define a base class Person
+class Person {
+    // Method to describe sleep habits of a normal human
+    getSleep() {
+        console.log('I am a normal human, so I regularly sleep 8 hours!');
     }
+};
 
-    const person1 = new Person();
-    const person2 = new Student();
-    const person3 = new Developer();
+// Define a subclass Student inheriting from Person
+class Student extends Person {
+    // Method to describe sleep habits of a student
+    getSleep() {
+        console.log('I am a student, so I regularly sleep 6 hours!');
+    }
+};
 
-    getSleepHours(person1);
-    getSleepHours(person2);
-    getSleepHours(person3);
+// Define a subclass Developer inheriting from Person
+class Developer extends Person {
+    // Method to describe sleep habits of a developer
+    getSleep() {
+        console.log('I am a developer, so I regularly sleep 4 hours!');
+    }
+};
+
+// Function to get sleep hours of a person
+const getSleepHours = (param: Person) => {
+    // Call the getSleep method of the provided Person object
+    param.getSleep();
+}
+
+// Create instances of Person, Student, and Developer classes
+const person1 = new Person();
+const person2 = new Student();
+const person3 = new Developer();
+
+// Call the getSleepHours function with different types of Person objects
+getSleepHours(person1); // Calls the getSleep method of Person class
+getSleepHours(person2); // Calls the getSleep method of Student class
+getSleepHours(person3); // Calls the getSleep method of Developer class
 
 
 
