fe1b7bd2326569007493b55cc0391ed876fcb73a
Md Rabby <ablazerabbi159@gmail.com>
2024-05-07 02:36:52 +0600

    added comment

--- oop/abstraction.ts
+++ oop/abstraction.ts
@@ -1,35 +1,44 @@
 {
-    //Abstraction in OOP
+ // Abstraction in OOP
 
-    interface Vehicle{
-        startEngine() : void;
-        stopEngine() : void;
-        move() : void;
+// Define an interface Vehicle specifying abstract methods for starting, stopping, and moving the vehicle
+interface Vehicle {
+    startEngine(): void;
+    stopEngine(): void;
+    move(): void;
+};
+
+// Define a class Bike implementing the Vehicle interface
+class Bike implements Vehicle {
+    // Implement the startEngine method of the Vehicle interface
+    startEngine(): void {
+        console.log('I am starting the engine.');
     };
 
-    class Bike implements Vehicle{
-        startEngine(): void {
-            console.log('I starting engine.');
-        };
-
-        stopEngine(): void {
-            console.log('I stoping engine');
-        };
-
-        move(){
-            console.log('I am running engine');
-        };
-
-        test(){
-            console.log('testing...');
-        }
+    // Implement the stopEngine method of the Vehicle interface
+    stopEngine(): void {
+        console.log('I am stopping the engine');
     };
 
-    const bike = new Bike();
-    bike.startEngine();
-    bike.stopEngine();
-    bike.move();
-    bike.test();
+    // Implement the move method of the Vehicle interface
+    move() {
+        console.log('I am running the engine');
+    };
+
+    // Additional method specific to the Bike class
+    test() {
+        console.log('Testing...');
+    }
+};
+
+// Create an instance of the Bike class
+const bike = new Bike();
+// Call the implemented methods
+bike.startEngine(); // Output: I am starting the engine.
+bike.stopEngine(); // Output: I am stopping the engine
+bike.move(); // Output: I am running the engine
+bike.test(); // Output: Testing...
+
 
 
 
